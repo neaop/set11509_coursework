7db2bcba4470dff12377b628cd912a228610d548
neaop <neaop@users.noreply.github.com>
2018-03-17 22:35:46 +0000

    Add user initialisation class

---
+++ src/main/java/user/UserInit.java
@@ -0,0 +1,32 @@
+package user;
+
+import user.controller.LoginController;
+import user.controller.RegisterController;
+import user.model.LoginModel;
+import user.model.RegisterModel;
+import user.view.UserView;
+
+public class UserInit {
+    public static void initaliseUserForm() {
+        LoginController loginController = new LoginController();
+        RegisterController registerController = new RegisterController();
+
+        LoginModel loginModel = new LoginModel();
+        RegisterModel registerModel = new RegisterModel();
+
+        UserView view = new UserView();
+
+        loginModel.addObserver(view);
+        registerModel.addObserver(view);
+
+        loginController.setModel(loginModel);
+        loginController.setView(view);
+
+        registerController.setModel(registerModel);
+        registerController.setView(view);
+
+        view.setRegisterController(registerController);
+        view.setLoginControl(loginController);
+
+    }
+}
