5e2198ce0f4c6b556a8b6e05c348c8af14fbd8b9
neaop <neaop@users.noreply.github.com>
2018-04-14 18:21:29 +0100

    Add comment to main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,17 @@ import global.GlobalController;
 import java.io.Serializable;
 import java.sql.SQLException;
 
+/**
+ * The entry point of the Share Trader application.
+ */
 public class Main implements Serializable {
 
+    /**
+     * Launch the application.
+     *
+     * @param args system arguments
+     * @throws SQLException if error with database instance or query
+     */
     public static void main(String[] args) throws SQLException {
         GlobalController globalController = new GlobalController();
         globalController.runApplication();
